7fa7e5aafa4caee413fb70a34f2b9bb9f7c58b9d
celestialmary <prime_proger@mail.ru>
2022-06-23 11:13:51 +0300

    Task 1_1

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (b == a * a)
+{
+    Console.WriteLine("Yes");    
+}
+else
+{
+    Console.WriteLine("No");
+}
