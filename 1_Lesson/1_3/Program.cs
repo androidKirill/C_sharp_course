44c86b2ca21c5ee08caeb009ca1de037922e6b42
celestialmary <prime_proger@mail.ru>
2022-06-27 09:09:33 +0300

    task_1_3

---
+++ 1_Lesson/1_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// 3. Напишите программу вычисления функции:
+//    y = f(x)
+
+string s_x = Console.ReadLine();
+int x = int.Parse(s_x);
+int y = -2 * (x * x) + 4 * x + 2;
+
+Console.WriteLine(y);
