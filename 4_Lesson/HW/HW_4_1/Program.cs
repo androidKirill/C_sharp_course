f58fbbfee5578fbe22a7d0b77fbb8aea6f938d15
celestialmary <prime_proger@mail.ru>
2022-07-07 09:47:34 +0300

    Task HW_4_1

---
+++ 4_Lesson/HW/HW_4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите цикл, который принимает на вход два числа (A и B)
+//    и возводит число A в натуральную степень B.
+
+
+int PowNum(int a, int b)
+{   
+    int n_pow = a;
+    
+    for (int i = 2; i < b; i++)
+    {
+        n_pow *= a; 
+    }  
+    return n_pow;  
+}
+
+Console.WriteLine(PowNum(3, 5));
+Console.WriteLine(PowNum(2, 4));
