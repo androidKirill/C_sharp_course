a30a51e1cd55d88c285cee82d78921861dd15243
celestialmary <prime_proger@mail.ru>
2022-07-07 09:48:15 +0300

    Task HW_4_2

---
+++ 4_Lesson/HW/HW_4_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// 2. Напишите программу, которая принимает
+//    на вход число и выдаёт сумму цифр в числе.
+
+
+int SumNums(int num)
+{
+    int n_sum = 0;
+
+    while (num > 0)
+    {
+        n_sum += num % 10;
+        num /= 10;
+    }
+    return n_sum;
+}
+
+
+// --------------------------------- 2 вариант
+
+int SumNums(int num)
+{
+    int n_sum = 0;
+
+    for (int i = num; i != 0; i /= 10)
+        n_sum += num % 10;
+
+    return n_sum;
+}
+
+Console.WriteLine(SumNums(452));
+Console.WriteLine(SumNums(82));
+Console.WriteLine(SumNums(9012));
