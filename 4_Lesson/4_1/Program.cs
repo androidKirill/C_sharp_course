ab3c36f505ad013b7ac9fe8af20098a1c0ccedbe
celestialmary <prime_proger@mail.ru>
2022-07-07 09:45:01 +0300

    Task 4_1

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+
+// 1 вариант
+
+int NumCount(int num)
+{
+    string result = num.ToString();
+    return result.Length;
+}
+
+Console.WriteLine(NumCount(456));
+Console.WriteLine(NumCount(78));
+Console.WriteLine(NumCount(89126));
